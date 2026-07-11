c19b3815eaab438968643e34007966f25bfb271c
Miguel Villanueva <miguelvillanueva@Miguels-MacBook-Pro.local>
2026-07-11 21:17:01 +0200

    basic main function, server working

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 mod models;
 mod routes;
+use tokio::net::{TcpListener};
+use tower_http::cors::CorsLayer;
+use axum::Router;
+use axum::routing::{get, post};
+use tower_http::services::ServeDir;
+use routes::{download,upload};
 
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    // Initialize logging
+    
+    // Build the routerx
+    let app = Router::new()
+        .route("/api/upload", post(upload))
+        .route("/api/download/{filename}", get(download))
+
+        .layer(CorsLayer::permissive());
+    
+    // Start the server
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
